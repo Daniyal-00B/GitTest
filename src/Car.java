1be33252535649167a07efc3548247fb4314fb66
Daniyal <disectioenist@gmail.com>
2025-06-01 21:02:04 +0330

    modify car method

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,6 @@
 public class Car {
     private String model;
     public void Drive(){
-        System.out.println("Car is Driving");
+        System.out.println("You Are Driving The Car");
     }
 }
